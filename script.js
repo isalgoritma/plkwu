a9beebd67ab14cd7b6d734338b64d8d4b139222d
isalgoritma <slsbilaa19@gmail.com>
2026-05-19 09:21:53 +0700

    update website plasthetic

--- script.js
+++ script.js
@@ -2,19 +2,31 @@ console.log("Plasthetic Website Ready");
 
 const navbar = document.querySelector(".navbar");
 
+let lastScroll = 0;
+
 window.addEventListener("scroll", () => {
+
+    const currentScroll = window.pageYOffset;
+
+    if(currentScroll > lastScroll){
+
+        navbar.style.transform = "translateY(-100%)";
+
+    } else {
+
+        navbar.style.transform = "translateY(0)";
+    }
+
+    lastScroll = currentScroll;
 
     if(window.scrollY > 50){
 
         navbar.style.boxShadow =
-        "0 5px 20px rgba(0,0,0,0.05)";
+        "0 5px 20px rgba(0,0,0,0.08)";
 
-    }
-
-    else{
+    } else {
 
         navbar.style.boxShadow = "none";
-
     }
 
 });
